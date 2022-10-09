6234ebffcdf1085c162102a86c2c44b4d3d9ab30
kpisti18 <k.istvanzoltan@gmail.com>
2022-10-09 12:42:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kereget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérek egy számot: ");
+            int a = int.Parse(Console.ReadLine());
+            List<int> szamok = new List<int>();
+
+            while (a != 0)
+            {
+                szamok.Add(a);
+                Console.Write("Kérek egy számot: ");
+                a = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("A számok:");
+            foreach (var item in szamok)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
